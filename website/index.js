c68d83264143f122eaa311a3e55400a7587f5b70
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-27 15:21:53 +0300

    type conversion and math practice code added

--- website/index.js
+++ website/index.js
@@ -1,7 +1,26 @@
-let username;
+//type conversion
 
-document.getElementById("my-submit").onclick = function () {
-  username = document.getElementById("my-text").value;
-  console.log(username);
-  document.getElementById("title-hello").textContent = `Hello ${username}`;
-};
+let age = window.prompt("How old are you?");
+age += 1;
+console.log(age);
+console.log("Age is a " + typeof age + " type.");
+console.log("---------------------");
+
+let ageNumber = window.prompt("How old are you?");
+ageNumber = Number(ageNumber);
+ageNumber += 1;
+console.log(ageNumber);
+console.log("AgeNumber is a " + typeof ageNumber + " type.");
+
+console.log("---------------------");
+
+let x = "pizza";
+x = Number(x);
+console.log(x, typeof x);
+
+//Math practice
+
+console.log(Math.PI);
+
+let x2 = 3.21;
+let;
