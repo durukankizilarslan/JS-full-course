748a0e4dd87f2b8c0decdc791d26ee5855cb32ab
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-27 17:46:12 +0300

    ternary operator and switch statement practiced

--- website/index.js
+++ website/index.js
@@ -18,9 +18,47 @@ let x = "pizza";
 x = Number(x);
 console.log(x, typeof x);
 
-//Math practice
+//Ternary Operator practice
 
-console.log(Math.PI);
+// let ageTernary = 21;
+// let message = age >= 18 ? "You're an adult" : "You're a minor";
+// console.log(message);
 
-let x2 = 3.21;
-let;
+// let time = 9;
+// let greeting = time < 12 ? "Good morning" : "Good afternoon";
+// console.log(greeting);
+
+// let isStudent = true;
+// let student = isStudent ? "You're a student" : "You're NOT a student";
+// console.log(student);
+
+//SWITCH STATEMENT PRACTICE
+
+let day = "pizza";
+
+switch (day) {
+  case 1:
+    console.log("It's Monday");
+    break;
+  case 2:
+    console.log("It's Tuesday");
+    break;
+  case 3:
+    console.log("It's Wednesday");
+    break;
+  case 4:
+    console.log("It's Thursday");
+    break;
+  case 5:
+    console.log("It's Friday");
+    break;
+  case 6:
+    console.log("It's Saturday");
+    break;
+  case 7:
+    console.log("It's Sunday");
+    break;
+
+  default:
+    console.log(`${day} is not a day`);
+}
