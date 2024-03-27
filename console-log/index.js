97a6d0cb28998c3ac0dd045be714b55f73ad46d3
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-27 22:57:46 +0300

    String slicing practiced

---
+++ console-log/index.js
@@ -0,0 +1,51 @@
+// STRING METHODS PRACTICE
+
+// let userName = "BroCode";
+
+// console.log(userName.charAt(0));
+// console.log(userName.indexOf("o"));
+// console.log(userName.lastIndexOf("o"));
+
+// userName = "BroCode     ";
+
+// console.log(userName.trim());
+
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+// console.log(userName.repeat(3));
+
+// console.log(userName.startsWith(" "));
+// console.log(userName.startsWith("B"));
+
+// console.log(userName.endsWith(" "));
+// console.log(userName.endsWith("e"));
+
+// console.log(userName.includes(" "));
+// console.log(userName.includes(","));
+
+// let phoneNumber = "123-456-78-90";
+// console.log(phoneNumber.replaceAll("-", "/"));
+
+// console.log(userName.padStart(15, "0"));
+// console.log(userName.padEnd(15, "0"));
+
+//STRING SLICING
+
+// const fullName = "Bro Code";
+
+// firstName = fullName.slice(0, fullName.indexOf(" "));
+
+// lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const email = "broCode1@gmail.com";
+
+// let userName = email.slice(0, email.indexOf("@"));
+// let extension = email.slice(email.indexOf("@") + 1);
+
+// console.log(userName);
+// console.log(extension);
+
+// CHAIN METHOD
