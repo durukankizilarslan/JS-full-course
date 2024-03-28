a579f690a63779695461d44064548b17149d3ae5
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-28 15:50:18 +0300

    input type checked code added

--- checked/index.js
+++ checked/index.js
@@ -1,1 +1,28 @@
-// COUNTER PROGRAM
+// CHECKED
+
+const myCheckbox = document.getElementById("my-checkbox");
+const visaBtn = document.getElementById("btn-visa");
+const paypalBtn = document.getElementById("btn-paypal");
+const mastercardBtn = document.getElementById("btn-mastercard");
+const mySubmit = document.getElementById("my-submit");
+
+const resultSub = document.getElementById("result-sub");
+const resultPayment = document.getElementById("result-payment");
+
+mySubmit.onclick = function () {
+  if (myCheckbox.checked) {
+    resultSub.textContent = "You're subscribed!";
+  } else {
+    resultSub.textContent = "You're NOT subscribed!";
+  }
+
+  if (visaBtn.checked) {
+    resultPayment.textContent = "You're paying with Visa.";
+  } else if (mastercardBtn.checked) {
+    resultPayment.textContent = "You're paying with Mastercard.";
+  } else if (paypalBtn.checked) {
+    resultPayment.textContent = "You're paying with PayPal.";
+  } else {
+    resultPayment.textContent = "You must select a payment type.";
+  }
+};
