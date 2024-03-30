f367f2b66a22aab67402589166ca0715639187dc
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-30 07:12:27 +0300

    spread and rest parameters practiced

--- console-log/index.js
+++ console-log/index.js
@@ -60,3 +60,59 @@
 
 // function1();
 // console.log(x);
+
+// SPREAD OPERATOR
+
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+
+// let username = "Bro Code";
+// let letters = [...username].join("-");
+
+// console.log(maximum);
+// console.log(minimum);
+// console.log(letters);
+
+// let fruits = ["apple", "orange"];
+// let vegetables = ["celery", "carrot"];
+
+// let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+// console.log(foods);
+
+// REST PARAMETERS
+
+function sum(...numbers) {
+  let result = 0;
+
+  for (let number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+const total = sum(1, 2, 3, 4, 5);
+console.log(`Your total is ${total}`);
+
+function average(...numbers) {
+  let result = 0;
+
+  for (let number of numbers) {
+    result += number;
+  }
+  return result / numbers.length;
+}
+
+const averageNumbers = average(1, 2, 3, 4, 5);
+console.log(`Your average is ${averageNumbers}`);
+
+// -----------------------------------------------------
+
+const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
+
+function combineStrings(...strings) {
+  return strings.join(" ");
+}
+
+console.log(fullName);
