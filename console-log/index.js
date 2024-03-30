c081764973138f4c45de8df07e3e468b9d5fc882
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-30 18:27:35 +0300

    practiced map, filter, and reduce methods

--- console-log/index.js
+++ console-log/index.js
@@ -83,36 +83,168 @@
 
 // REST PARAMETERS
 
-function sum(...numbers) {
-  let result = 0;
+// function sum(...numbers) {
+//   let result = 0;
 
-  for (let number of numbers) {
-    result += number;
-  }
-  return result;
-}
+//   for (let number of numbers) {
+//     result += number;
+//   }
+//   return result;
+// }
 
-const total = sum(1, 2, 3, 4, 5);
-console.log(`Your total is ${total}`);
+// const total = sum(1, 2, 3, 4, 5);
+// console.log(`Your total is ${total}`);
 
-function average(...numbers) {
-  let result = 0;
+// function average(...numbers) {
+//   let result = 0;
 
-  for (let number of numbers) {
-    result += number;
-  }
-  return result / numbers.length;
-}
+//   for (let number of numbers) {
+//     result += number;
+//   }
+//   return result / numbers.length;
+// }
 
-const averageNumbers = average(1, 2, 3, 4, 5);
-console.log(`Your average is ${averageNumbers}`);
+// const averageNumbers = average(1, 2, 3, 4, 5);
+// console.log(`Your average is ${averageNumbers}`);
 
-// -----------------------------------------------------
+// // -----------------------------------------------------
 
-const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
+// const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
 
-function combineStrings(...strings) {
-  return strings.join(" ");
-}
+// function combineStrings(...strings) {
+//   return strings.join(" ");
+// }
 
-console.log(fullName);
+// console.log(fullName);
+
+// forEach() PRACTICE
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach(cube);
+// numbers.forEach(display);
+
+// function double(element, index, array) {
+//   array[index] = element * 2;
+// }
+
+// function triple(element, index, array) {
+//   array[index] = element * 3;
+// }
+
+// function square(element, index, array) {
+//   array[index] = Math.pow(element, 2);
+// }
+
+// function cube(element, index, array) {
+//   array[index] = Math.pow(element, 3);
+// }
+
+// function display(element) {
+//   console.log(element);
+// }
+
+// let fruits = ["apple", "banana", "orange", "coconut"];
+
+// fruits.forEach(upperCase);
+// fruits.forEach(display);
+
+// function upperCase(element, index, array) {
+//   array[index] = element.toUpperCase();
+// }
+
+// function display(element) {
+//   console.log(element);
+// }
+
+//.map() method practice
+
+// const numbers = [1, 2, 3, 4, 5];
+// const squares = numbers.map(square);
+// const cubes = numbers.map(cube);
+
+// console.log(cubes);
+
+// function square(element) {
+//   return Math.pow(element, 2);
+// }
+
+// function cube(element) {
+//   return Math.pow(element, 3);
+// }
+
+// // ================================
+
+// const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+
+// const studentsUpper = students.map(upperCase);
+// const studentsLower = students.map(lowerCase);
+// console.log(studentsUpper);
+// console.log(studentsLower);
+
+// function upperCase(element) {
+//   return element.toUpperCase();
+// }
+
+// function lowerCase(element) {
+//   return element.toLowerCase();
+// }
+
+// const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+// const formattedDates = dates.map(formatDates);
+
+// console.log(formattedDates);
+
+// function formatDates(element) {
+//   const parts = element.split("-");
+//   return `${parts[1]}/${parts[2]}/${parts[0]}`;
+// }
+
+//----------
+
+//filter() method practice
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+// const evenNumbers = numbers.filter(isEven);
+// const oddNumbers = numbers.filter(isOdd);
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+// function isEven(element) {
+//   return element % 2 === 0;
+// }
+
+// function isOdd(element) {
+//   return element % 2 !== 0;
+// }
+
+//reduce METHOD PRACTICE
+
+// const prices = [5, 30, 10, 25, 15, 20];
+
+// const total = prices.reduce(sum);
+
+// console.log(`${total.toFixed(2)}`);
+
+// function sum(accumulator, element) {
+//   return accumulator + element;
+// }
+
+// const grades = [75, 50, 90, 80, 65, 95];
+
+// const maximum = grades.reduce(getMax);
+// const minimum = grades.reduce(getMin);
+
+// console.log(maximum);
+// console.log(minimum);
+
+// function getMax(accumulator, element) {
+//   return Math.max(accumulator, element);
+// }
+
+// function getMin(accumulator, element) {
+//   return Math.min(accumulator, element);
+// }
+
+// ARROW FUNCTIONS
