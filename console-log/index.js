e3a25241626903969079a0aa2123ba7c0378db84
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-31 10:30:15 +0300

    arrow functions practiced

--- console-log/index.js
+++ console-log/index.js
@@ -248,3 +248,15 @@
 // }
 
 // ARROW FUNCTIONS
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const oddNums = numbers.filter((element) => element % 2 === 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+console.log(squares);
+console.log(cubes);
+console.log(oddNums);
+console.log(total);
