ff9cf5ee195e578cd2d45579f919958881f129cc
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-04-01 20:05:55 +0300

    Getters and setters practiced

--- console-log/index.js
+++ console-log/index.js
@@ -566,3 +566,105 @@
 // hawk.fly();
 
 // ---------------------------------------------------------------
+
+// Getters and Setters
+
+// class Rectangle {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   set width(newWidth) {
+//     if (newWidth > 0) {
+//       this._width = newWidth;
+//     } else {
+//       console.error("Width must be a positive number");
+//     }
+//   }
+
+//   set height(newHeight) {
+//     if (newHeight > 0) {
+//       this._height = newHeight;
+//     } else {
+//       console.error("Height must be a positive number");
+//     }
+//   }
+
+//   get width() {
+//     return `${this._width.toFixed(2)} cm`;
+//   }
+
+//   get height() {
+//     return `${this._height.toFixed(1)} cm`;
+//   }
+
+//   get area() {
+//     return `${(this._width * this._height).toFixed(1)} cm2`;
+//   }
+// }
+
+// const rectangle = new Rectangle(2, 4);
+
+// rectangle.width = 5;
+// rectangle.height = 2;
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+// // ---------------------------------
+
+// class Person {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
+
+//   set firstName(newFirstName) {
+//     if (typeof newFirstName === "string" && newFirstName.length > 0) {
+//       this._firstName = newFirstName;
+//     } else {
+//       console.error("First name must be an non-empty string");
+//     }
+//   }
+
+//   set lastName(newLastName) {
+//     if (typeof newLastName === "string" && newLastName.length > 0) {
+//       this._lastName = newLastName;
+//     } else {
+//       console.error("Last name must be an non-empty string");
+//     }
+//   }
+
+//   set age(newAge) {
+//     if (typeof newAge === "number" && newAge > 0) {
+//       this._age = newAge;
+//     } else {
+//       console.error("Age must be a positive number");
+//     }
+//   }
+
+//   get firstName() {
+//     return this._firstName;
+//   }
+//   get lastName() {
+//     return this._lastName;
+//   }
+
+//   get fullName() {
+//     return this._firstName + " " + this._lastName;
+//   }
+
+//   get age() {
+//     return this._age;
+//   }
+// }
+
+// const person = new Person("Spongebob", "Squarepants", 30);
+
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+// console.log(person.fullName);
